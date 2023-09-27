b5d2dd4c83a46d2a69ba2627f4b4aef12956cf9d
Alwin-13 <145443630+Alwin-13@users.noreply.github.com>
2023-09-27 02:20:35 -0700

    Floatingnumber.java

--- Floatingnumber.java
+++ Floatingnumber.java
@@ -4,18 +4,18 @@ public class Floatingnumber {
 	public static void main()
 	{
 Scanner sc=new Scanner(System.in);
-float number;
+double number;
 System.out.println("Enter any number");
- number=sc.nextFloat();
+ number=sc.nextDouble();
  if(number==0)
 	 System.out.println("Number is Zero");
  else if(number>0)
 	 System.out.println("Number is positive");
  else if(number<0)
 	 System.out.println("Number is negative");
- else if(number<1)
+ else if(Math.abs(number)<1)
 	 System.out.println("Number is small");
- else if(number>1000000)
+ else if(Math.abs(number)>1000000)
 	 System.out.println("Number is large");
  else
 	 System.out.println("Entered character is not a number");
